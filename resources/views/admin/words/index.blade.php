fd6be8adf43357403464445c0960d366641702ec
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-05 15:26:14 +0200

    refactoring admin index

--- resources/views/admin/words/index.blade.php
+++ resources/views/admin/words/index.blade.php
@@ -6,23 +6,25 @@
 
     <table class="table mt-5">
         <thead>
-            <tr>
+            <tr class="align-middle text-center">
                 <th scope="col">#</th>
                 <th scope="col">Titolo</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Descrizione</th>
                 <th scope="col">Creato il</th>
                 <th scope="col">Modificato il</th>
-                <th class="d-flex justify-content-end">
-                    <a href="{{ route('admin.words.create') }}" class="btn btn-success">
-                        <i class="fas fa-plus me-2"></i>Aggiungi Termine
-                    </a>
+                <th>
+                    <div class="d-flex justify-content-end">
+                        <a href="{{ route('admin.words.create') }}" class="btn btn-success">
+                            <i class="fas fa-plus me-2"></i>Aggiungi Termine
+                        </a>
+                    </div>
                 </th>
             </tr>
         </thead>
         <tbody>
             @forelse ($words as $word)
-                <tr>
+                <tr class="align-middle text-center">
                     <th scope="row">{{ $word->id }}</th>
                     <td>{{ $word->title }}</td>
                     <td>{{ $word->slug }}</td>
@@ -30,8 +32,8 @@
                     <td>{{ $word->getFormattedDate('created_at') }}</td>
                     <td>{{ $word->getFormattedDate('updated_at') }}</td>
 
-                    <td class="d-flex justify-content-end">
-                        <div class="d-flex gap-2">
+                    <td>
+                        <div class="d-flex justify-content-end gap-2">
                             <a href="{{ route('admin.words.show', $word) }}" class="btn btn-sm btn-primary"><i
                                     class="fa-solid fa-eye"></i></a>
 

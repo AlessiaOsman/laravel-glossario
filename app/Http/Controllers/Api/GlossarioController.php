33006de6f3ef33bcac78a9ed48ccea7b74f18922
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-06 12:02:14 +0200

    added api GlossarioController.php

---
+++ app/Http/Controllers/Api/GlossarioController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Word;
+use Illuminate\Http\Request;
+
+class GlossarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $words = Word::orderByDesc('updated_at')->orderByDesc('created_at')->paginate(10)->withQueryString();
+
+        return response()->json($words);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $slug)
+    {
+        $word = Word::whereSlug($slug)->with('tags')->first();
+        if (!$word) return response(null, 404);
+        return response()->json($word);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Word $word)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Word $word)
+    {
+        //
+    }
+}
